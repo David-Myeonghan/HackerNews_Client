014c9223af448fd01cc2beee3bab3435759dcb37
David MH Ryu <myeonghan12@gmail.com>
2021-08-17 23:59:32 +0900

    pagination

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const container = document.getElementById('root');
-let ajax = new XMLHttpRequest();
+const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+const store = {
+	currentPage: 1,
+};
 
 function getData(url) {
 	ajax.open('GET', url, false);
@@ -17,28 +20,33 @@ function newsFeed() {
 
 	newsList.push('<ul>');
 
-	for (let i = 0; i < 10; i++) {
+	for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
 		newsList.push(
 			`<li>
-            <a href='#${newsFeed[i].id}'>
+            <a href='#/show/${newsFeed[i].id}'>
                 ${newsFeed[i].title} (${newsFeed[i].comments_count})
             </a>
         </li>`
 		);
 	}
 	newsList.push('</ul>');
+	newsList.push(`
+    <div>
+        <a href='#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}'>이전 페이지</a>
+        <a href='#/page/${store.currentPage + 1}'>다음 페이지</a>
+    </div>`);
 	container.innerHTML = newsList.join('');
 }
 
 function newsDetail() {
-	const id = location.hash.substr(1);
+	const id = location.hash.substr(7);
 	const newsContent = getData(CONTENT_URL.replace('@id', id));
 
 	container.innerHTML = `
         <h1>${newsContent.title}</h1>
 
         <div>
-            <a href='#'>목목으로</a>
+            <a href='#/page/${store.currentPage}'>목목으로</a>
         </div>
     `;
 }
@@ -49,6 +57,9 @@ function router() {
 	// hash에 '#' 만 들어있으면 빈 값으로 인식.
 	if (routePath === '') {
 		newsFeed();
+	} else if (routePath.indexOf('#/page/') >= 0) {
+		store.currentPage = Number(routePath.substr(7));
+		newsFeed();
 	} else {
 		newsDetail();
 	}
@@ -57,3 +68,4 @@ function router() {
 window.addEventListener('hashchange', router);
 
 router();
+console.log(store.currentPage);
