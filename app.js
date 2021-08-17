2df31ede4996475cb6ab57b0405adb6f78b1fab5
David <myeonghan12@gmail.com>
2021-08-17 17:48:09 +0900

    dom 제거 및 문자열을 가지고 ui 구성

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const container = document.getElementById('root');
 let ajax = new XMLHttpRequest();
+const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
 ajax.open('GET', NEWS_URL, false);
 ajax.send();
@@ -7,12 +10,34 @@ ajax.send();
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement('ul');
 
+window.addEventListener('hashchange', function () {
+    const id = location.hash.substr(1);
+    ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+    ajax.send();
+
+    const newsContent = JSON.parse(ajax.response);
+    const title = document.createElement('h1');
+
+    title.innerHTML = newsContent.title;
+    content.appendChild(title);
+});
+
 for (let i = 0; i < 10; i++) {
+    const div = document.createElement('div');
     const li = document.createElement('li');
+    const a = document.createElement('a');
 
-    li.innerHTML = newsFeed[i].title;
+    div.innerHTML = `
+    <li>
+        <a href='#${newsFeed[i].id}'>
+        ${newsFeed[i].title} (${newsFeed[i].comments_count})
+        </a>
+    </li>
+    `;
 
-    ul.appendChild(li);
+    ul.appendChild(div.firstElementChild);
+    ul.appendChild(div.children[0]);
 }
 
-document.getElementById('root').appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);
