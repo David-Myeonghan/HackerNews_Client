bb8596007e5b2cabacb945f4c4ab7bb878f436ac
David <myeonghan12@gmail.com>
2021-08-17 18:47:46 +0900

    refactoring

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
+function getData(url) {
+    ajax.open('GET', url, false);
+    ajax.send();
 
-const newsFeed = JSON.parse(ajax.response);
+    return JSON.parse(ajax.response);
+}
+
+const newsFeed = getData(NEWS_URL);
 const ul = document.createElement('ul');
 
 window.addEventListener('hashchange', function () {
     const id = location.hash.substr(1);
-    ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+    ajax.open('GET', , false);
     ajax.send();
 
-    const newsContent = JSON.parse(ajax.response);
+    const newsContent = getData(CONTENT_URL.replace('@id', id));
     const title = document.createElement('h1');
 
     title.innerHTML = newsContent.title;
