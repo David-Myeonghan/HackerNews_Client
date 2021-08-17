aaea3770c300e9cc10a602c5f40a034203d83a3f
David MH Ryu <myeonghan12@gmail.com>
2021-08-17 23:13:06 +0900

    fix bug

--- app.js
+++ app.js
@@ -5,33 +5,31 @@ const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
 function getData(url) {
-    ajax.open('GET', url, false);
-    ajax.send();
+	ajax.open('GET', url, false);
+	ajax.send();
 
-    return JSON.parse(ajax.response);
+	return JSON.parse(ajax.response);
 }
 
 const newsFeed = getData(NEWS_URL);
 const ul = document.createElement('ul');
 
 window.addEventListener('hashchange', function () {
-    const id = location.hash.substr(1);
-    ajax.open('GET', , false);
-    ajax.send();
+	const id = location.hash.substr(1);
 
-    const newsContent = getData(CONTENT_URL.replace('@id', id));
-    const title = document.createElement('h1');
+	const newsContent = getData(CONTENT_URL.replace('@id', id));
+	const title = document.createElement('h1');
 
-    title.innerHTML = newsContent.title;
-    content.appendChild(title);
+	title.innerHTML = newsContent.title;
+	content.appendChild(title);
 });
 
 for (let i = 0; i < 10; i++) {
-    const div = document.createElement('div');
-    const li = document.createElement('li');
-    const a = document.createElement('a');
+	const div = document.createElement('div');
+	const li = document.createElement('li');
+	const a = document.createElement('a');
 
-    div.innerHTML = `
+	div.innerHTML = `
     <li>
         <a href='#${newsFeed[i].id}'>
         ${newsFeed[i].title} (${newsFeed[i].comments_count})
@@ -39,8 +37,8 @@ for (let i = 0; i < 10; i++) {
     </li>
     `;
 
-    ul.appendChild(div.firstElementChild);
-    ul.appendChild(div.children[0]);
+	ul.appendChild(div.firstElementChild);
+	// ul.appendChild(div.children[0]);
 }
 
 container.appendChild(ul);
