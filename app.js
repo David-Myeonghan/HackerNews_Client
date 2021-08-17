f8cfdb87638597bb8079f6535fd51318f860d65b
David MH Ryu <myeonghan12@gmail.com>
2021-08-17 23:30:19 +0900

    라우터 만들기 위해 함수화

--- app.js
+++ app.js
@@ -11,35 +11,38 @@ function getData(url) {
 	return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement('ul');
+function newsFeed() {
+	const newsFeed = getData(NEWS_URL);
+	const newsList = [];
 
-window.addEventListener('hashchange', function () {
+	newsList.push('<ul>');
+
+	for (let i = 0; i < 10; i++) {
+		newsList.push(
+			`<li>
+            <a href='#${newsFeed[i].id}'>
+                ${newsFeed[i].title} (${newsFeed[i].comments_count})
+            </a>
+        </li>`
+		);
+	}
+	newsList.push('</ul>');
+	container.innerHTML = newsList.join('');
+}
+
+function newsDetail() {
 	const id = location.hash.substr(1);
 
 	const newsContent = getData(CONTENT_URL.replace('@id', id));
 	const title = document.createElement('h1');
 
-	title.innerHTML = newsContent.title;
-	content.appendChild(title);
-});
+	container.innerHTML = `
+        <h1>${newsContent.title}</h1>
 
-for (let i = 0; i < 10; i++) {
-	const div = document.createElement('div');
-	const li = document.createElement('li');
-	const a = document.createElement('a');
-
-	div.innerHTML = `
-    <li>
-        <a href='#${newsFeed[i].id}'>
-        ${newsFeed[i].title} (${newsFeed[i].comments_count})
-        </a>
-    </li>
+        <div>
+            <a href='#'>목목으로</a>
+        </div>
     `;
-
-	ul.appendChild(div.firstElementChild);
-	// ul.appendChild(div.children[0]);
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+window.addEventListener('hashchange', newsDetail);
